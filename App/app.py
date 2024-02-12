13da4ef5b710bddf20a3dc8d4a561acf02d183b1
Nick <nick@figrs.com>
2024-02-12 13:42:03 +0100

    Update file path in app.py

--- App/app.py
+++ App/app.py
@@ -28,7 +28,9 @@ tdf_logo = 'https://static.wixstatic.com/media/35c2c9_f5b19d5824844a429b91681561
 # -----------------------------------------------------------
 # Load the file
 cd = os.getcwd()
-file = f"{cd}/20240208_tdf_dmu.xlsx"
+print(cd)
+file = f"{cd}/App/20240208_tdf_dmu.xlsx"
+print(file)
 # file = "/Users/nick/Documents/dev/google_search/the_digital_federation/streamlit/App/20240208_tdf_dmu.xlsx"
 
 # Open the file
